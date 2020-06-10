9f98bee597b0732a202c41497732a8348120d585
Bibek Adhikari <62512236+bibek2141@users.noreply.github.com>
2020-06-10 13:45:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Function expression to select elements
+
+/*Navbar*/
+const selectElement = (s) => document.querySelector(s);
+
+//open the menu when clicked
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
+});
+
+//close the menu when clicked
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
+});
